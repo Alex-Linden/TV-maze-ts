f36156daddc7de5d8677a7959c35ca111f2e2749
Alex Linden <alextl2004@gmail.com>
2023-02-15 15:24:28 -0800

    type searchshowsbyterm

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,7 +5,12 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-
+interface ShowsInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: string | null;
+}
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -13,7 +18,7 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string): Promise<ShowsInterface[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
   return [
     {
@@ -30,20 +35,20 @@ async function getShowsByTerm(term) {
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
     }
-  ]
+  ];
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: ) {
   $showsList.empty();
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
               src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
@@ -60,7 +65,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
